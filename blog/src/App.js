e4438a77abeb8650475bfd48df1edb5f8bfe30f7
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-06 15:13:08 +0900

    JSX문법 3가지: className, 변수명{}, style={{이름:'값'}}

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+//1. class대신 className
+//2. 변수 넣을땐 {}
+//3. style 넣을땐 style={}
+// font-size => fontSize 카멜케이스로 변경
 
 function App() {
+  let post = "강남 우동 맛집";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>표자의 블로그</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
